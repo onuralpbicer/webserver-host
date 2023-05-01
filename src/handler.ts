4c3b0d3a0f686eb49c461cf0ad64ddaa87696233
Onur Alp Bicer <onuralpbicer@gmail.com>
2023-05-01 22:28:52 +1000

    Watch for the projects folder and read the port from the .port file

--- src/handler.ts
+++ src/handler.ts
@@ -2,9 +2,30 @@ import http from 'http'
 import path from 'path'
 import { getSubdomain, getFilePath, isWithinProject } from './utils'
 import { streamFileToResponse } from './stream'
+import fs from 'fs'
+import chokidar from 'chokidar'
 
 const baseLocation =
     process.env['BASE_LOCATION'] ?? path.join(process.cwd(), 'apps')
+const portFile = process.env['PORT_FILE'] ?? '.port'
+
+const baseApiPath = process.env['BASE_API_PATH'] ?? 'api'
+
+// Watch for the projects folder and read the port from the .port file
+const projectPortMap = new Map<string, number>()
+chokidar.watch(baseLocation).on('all', (eventName, filename) => {
+    if (!filename.endsWith(portFile)) return
+
+    const projectName = filename.split(path.sep).at(-2)
+    if (!projectName) return
+
+    console.log(eventName, projectName)
+    if (eventName.startsWith('unlink')) projectPortMap.delete(projectName)
+    else {
+        const port = fs.readFileSync(filename, { encoding: 'utf-8' })
+        if (port) projectPortMap.set(projectName, Number(port))
+    }
+})
 
 const handler: http.RequestListener = (req, res) => {
     const projectFolder = getSubdomain(req.headers.host ?? '') || 'index'
